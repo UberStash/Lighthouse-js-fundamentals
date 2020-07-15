c486c5c0166c9b7c3ddcfa8f1182affe6aeea734
UberStash <expiredserver@gmail.com>
2020-07-15 14:10:19 +0000

    updated boot camp prep files all completed minus the final stretch which you have started the html but have not started on the logic.

---
+++ final-stretch/app.js
@@ -0,0 +1,18 @@
+{/* <div id="form"><form action="">
+   <h3>Tell me your title.</h3>
+    <input type="text" id="title">
+    <h3>What shall we call your horizontal axis....and dont say 'evil'.</h3>
+   <input type="text" id="h-label">
+   <h3>What title shall be yer vertical title?</h3>
+   <input type="text" id="v-label">
+   <h3>Label it up B!!!!</h3>
+   <input type="text" id="data-labels">
+   <h3>How many bars of golden graphery would you like?</h3>
+   <input type="number" id="num-of-bars">
+   <h3>FEED ME YOUR DATA!!! (copy and paste please).</h3> 
+   <input type="number" id="data">
+   <h3>and ...... the range if its not too much to ask?</h3>
+   <input type="range" id='range'>
+  </form> */}
+
+  console.log(document.getElementById('title'))
--- kata-18/app.js
+++ kata-18/app.js
@@ -1,1 +1,55 @@
-prompt('what is your Guess?');
+
+let prompt = require("prompt-sync")();
+
+
+
+let guessedAlready = false;
+let listGuess = [];
+const answer = 25;
+let guess;
+
+const compare = function (answer) {
+  alert('INSTRUCTIONS --- Enter a number to guess the MAGIC NUMBER....You have 5 tries so makem count!');
+  for (i = 0; i < 5; i++) {
+    guess = (prompt('what is your Guess?'));
+    if (guess === 'exit') {
+      return;
+    }
+    guess = parseInt(guess);
+
+
+
+    if (guess === answer) {
+      alert(`You are correct!!! It took you ${listGuess.length + 1} Times`);
+      return;
+    } else if (isNaN(guess)) {
+      alert('Please Guess a Number!!!');
+    } else {
+      for (let i = 0; i < listGuess.length; i++) {
+        if (listGuess[i] === guess) {
+          alert('you already guessed that');
+          guessedAlready = true;
+        } 
+        
+      }
+            if (guess > answer && guessedAlready === false) {
+            alert('Too High Guess Again HAHAHAHAHA!!!!');
+            
+          } else if (guess < answer && guessedAlready === false) {
+            alert('Too Low Guess Again HAHAHAHAHA!!!!');
+            
+          }
+    }
+
+
+    listGuess.push(guess);
+
+  }
+
+  alert('You are out of turns GAME OVER!');
+
+}
+
+
+
+compare(25);
